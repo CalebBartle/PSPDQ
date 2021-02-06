f649995b7b59fba1597703cd54203f418198bf81
CalebBartle <56710703+CalebBartle@users.noreply.github.com>
2021-02-06 15:31:27 -0600

    Add files via upload
    <#
        .SYNOPSIS
            Sets PDQ Variable information
        
        .DESCRIPTION
            Sets PDQ Custom Variable information
        
        .PARAMETER Name
            Specifies the name of the variable to set for the custom variable.
    
        .PARAMETER Value
            Specifies the value of the variable to set for the custom variable.
    
        .EXAMPLE
            Set-PDQVariable -Name "GoogleChromeVersion" -Value '80.1.1.2'
        
            VarNumber   : 2
            VarName     : GoogleChromeVersion
            Value       : 80.1.1.2
            TimeCreated : 2021-02-06 14:15:07
            LastUpdated : 2021-02-06 14:15:29
    
        .NOTES
            Author: Caleb Bartle
    #>

---
+++ public/Set-PDQVariable.ps1
@@ -0,0 +1,39 @@
+﻿function Set-PDQVariable {
+ <#
+    .SYNOPSIS
+        Sets PDQ Variable information
+    
+    .DESCRIPTION
+        Sets PDQ Custom Variable information
+    
+    .PARAMETER Name
+        Specifies the name of the variable to set for the custom variable.
+
+    .PARAMETER Value
+        Specifies the value of the variable to set for the custom variable.
+
+    .EXAMPLE
+        Set-PDQVariable -Name "GoogleChromeVersion" -Value '80.1.1.2'
+    
+        VarNumber   : 2
+        VarName     : GoogleChromeVersion
+        Value       : 80.1.1.2
+        TimeCreated : 2021-02-06 14:15:07
+        LastUpdated : 2021-02-06 14:15:29
+
+    .NOTES
+        Author: Caleb Bartle
+#>
+    [CmdletBinding(SupportsShouldProcess = $True)]
+    param (
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'Name')]
+        [string]$Name,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Value
+    )
+    PDQDeploy.exe UpdateCustomVariable -Name $Name -Value $Value
+
+    Get-PDQVariable -Name $Name
+}
